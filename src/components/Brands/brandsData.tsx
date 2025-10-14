276c0ae37549b55905488f928fe92055e7a672a6
wondeks <162971992+wondeks@users.noreply.github.com>
2025-10-14 17:38:29 +0300

    Fix ESLint unescaped entitiesx rsssussle

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -1,19 +1,48 @@
 import { Brand } from "@/types/brand";
 
-// Import all images as StaticImports
-import formbold from "/public/images/brands/formbold.svg";
-import formboldLight from "/public/images/brands/formbold-light.svg";
-import uideck from "/public/images/brands/uideck.svg";
-import uideckLight from "/public/images/brands/uideck-light.svg";
-import tailgrids from "/public/images/brands/tailgrids.svg";
-import tailgridsLight from "/public/images/brands/tailgrids-light.svg";
-import lineicons from "/public/images/brands/lineicons.svg";
-import lineiconsLight from "/public/images/brands/lineicons-light.svg";
-import tailadmin from "/public/images/brands/tailadmin.svg";
-import tailadminLight from "/public/images/brands/tailadmin-light.svg";
-import plainadmin from "/public/images/brands/plainadmin.svg";
-import plainadminLight from "/public/images/brands/plainadmin-light.svg";
-
-export const brandsData: Brand[] = [
-  
+const brandsData: Brand[] = [
+  {
+    id: 6,
+    name: "Formbold",
+    href: "https://formbold.com",
+    image: "/images/brands/formbold.svg",
+    imageLight: "/images/brands/formbold-light.svg",
+  },
+  {
+    id: 1,
+    name: "UIdeck",
+    href: "https://uideck.com",
+    image: "/images/brands/uideck.svg",
+    imageLight: "/images/brands/uideck-light.svg",
+  },
+  {
+    id: 2,
+    name: "Tailgrids",
+    href: "https://tailgrids.com",
+    image: "/images/brands/tailgrids.svg",
+    imageLight: "/images/brands/tailgrids-light.svg",
+  },
+  {
+    id: 3,
+    name: "Lineicons",
+    href: "https://lineicons.com",
+    image: "/images/brands/lineicons.svg",
+    imageLight: "/images/brands/lineicons-light.svg",
+  },
+  {
+    id: 4,
+    name: "Tailadmin",
+    href: "https://tailadmin.com",
+    image: "/images/brands/tailadmin.svg",
+    imageLight: "/images/brands/tailadmin-light.svg",
+  },
+  {
+    id: 5,
+    name: "PlainAdmin",
+    href: "https://plainadmin.com",
+    image: "/images/brands/plainadmin.svg",
+    imageLight: "/images/brands/plainadmin-light.svg",
+  },
 ];
+
+export default brandsData;
